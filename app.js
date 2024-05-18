5df7320c5e15dc790336c6153e28cf1097a9688f
haslan82 <haslan82@hotmail.com>
2024-05-18 19:06:44 +0300

    ekleme-yaptım

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const API_URL =
-  'https://api.themoviedb.org/3/movie/157336?api_key=accbcfc7b4176186d61e12c389d6cded&append_to_response=videos'
+  'https://api.themoviedb.org/3/movie/550?api_key=accbcfc7b4176186d61e12c389d6cded'
+  
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API =
-  'https://api.themoviedb.org/4/search/movie?api_key=accbcfc7b4176186d61e12c389d6cdedc&query="'
+  'https://api.themoviedb.org/3/movie/550?api_key=accbcfc7b4176186d61e12c389d6cded'
 
   const form = document.getElementById("form");
   const search = document.getElementById("search");
@@ -16,5 +17,57 @@ getMovies(API_URL);
     const data = await res.json();
 
     console.log(data.results);
-    showMovies(data.results)
+    showMovies(data.results);
+    
   };
+
+  form.addEventListener("submit", (e)=> {
+    e.preventDefault()
+
+const searchTerm = search.value
+if(searchTerm && searchTerm!== ""){
+  getMovies(SEARCH_API + searchTerm)
+
+search.value= ''
+}else{
+  window.location.reload()
+}
+
+  })
+
+  function showMovies(movies){
+    main.innerHTML=""
+    movies.forEach((movie)=>{
+      const {title, poster_path, overview, vote_average} = movie
+      const movieEl = document.createElement("div")
+      movieEl.classList.add("movie")
+      movieEl.innerHTML=`
+      <img
+          src="${IMG_PATH + poster_path} "
+          alt="${title}"
+        />
+
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)} ">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>${title} <span>overview</span></h3>
+            <p>
+               ${overview}
+            </p>
+        </div>
+      `
+      main.appendChild(movieEl)
+    })
+  }
+
+ function getClassByRate(vote){
+  if(vote >=8) {
+    return "green"
+  }else if (vote >=5){
+    return 'orange'
+  }else {
+    return 'red'
+  }
+ }
