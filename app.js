46cab9f390e0d09e5196fe983d7e9f7a1259a9e0
haslan82 <haslan82@hotmail.com>
2024-05-18 17:57:27 +0300

    eklendi

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const API_URL =
-  'https:api.themoviedb.org/4/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+  'https://api.themoviedb.org/3/movie/157336?api_key=accbcfc7b4176186d61e12c389d6cded&append_to_response=videos'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API =
-  'https://api.themoviedb.org/4/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+  'https://api.themoviedb.org/4/search/movie?api_key=accbcfc7b4176186d61e12c389d6cdedc&query="'
+
+  const form = document.getElementById("form");
+  const search = document.getElementById("search");
+  const main = document.getElementById("main");
+
+
+getMovies(API_URL);
+
+  async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data.results);
+    showMovies(data.results)
+  };
